8a410627f251dedcc90762703c083ff02caff4be
Caroline Silva <carolinedsd@hotmail.com>
2024-10-02 14:56:04 -0300

    template e view do servidor

--- backend/dependencias_app/forms.py
+++ backend/dependencias_app/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.forms import ModelForm
 from dependencias_app.models.aluno import Aluno
 
+from dependencias_app.models.servidor import Servidor
+
 class AlunoForm(ModelForm):
     class Meta:
         model = Aluno
@@ -10,3 +12,7 @@ class AlunoForm(ModelForm):
             'dataNascimento': forms.DateInput(format='%d-%m-%Y', attrs={'type': 'date'}),
         }
 
+class ServidorForm(ModelForm):
+    class Meta:
+        model = Servidor
+        fields = '__all__'
--- backend/dependencias_app/views.py
+++ backend/dependencias_app/views.py
@@ -2,6 +2,8 @@ from dependencias_app.models.aluno import Aluno
 from dependencias_app.forms import AlunoForm
 from django.shortcuts import render, get_object_or_404, redirect, render
 
+from dependencias_app.forms import ServidorForm
+
 
 def create(request):
     if request.method == 'POST':
@@ -20,3 +22,16 @@ def list_alunos(request):
     alunos = Aluno.objects.all()  # Busca todos os alunos
     context = {'alunos': alunos}
     return render(request, 'aluno/list_aluno.html', context)  # Crie o template list.html
+
+def cadastrar_servidor(request):
+    if request.method == 'POST':
+        form = ServidorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('servidor_cadastrado')
+    else:
+        form = ServidorForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'servidor/cadastrar_servidor.html', context)
