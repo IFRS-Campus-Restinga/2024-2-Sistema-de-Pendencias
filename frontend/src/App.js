5ce72cb136dc35e3f6fd7bdac0b656f6ffbfa7a2
Cassio Renan <cassio.rr.de.paula@gmail.com>
2024-10-03 23:11:45 -0300

    revisão do roteador, app passou os componentes para home.js, retornando agora apenas o roteador

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,9 @@
-import React, { useState } from "react";
-import Button from "./components/Button/Button";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import BackButton from "./components/BackButton/BackButton";
-import Modal from "./components/Modal/Modal";
-import Dropdown from "./components/Dropdown/Dropdown";
-import Input from "./components/Input/Input";
-import "./../src/App.css";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import router from "./routes";
 
 const App = () => {
-  const [modalAberto, setModalAberto] = useState(false);
-
-  const abrirModal = () => setModalAberto(true);
-  const fecharModal = () => setModalAberto(false);
-
-  const itensDropdown = ["Plano de estudos", "Atividades", "Finalização"];
-  const handleBack = () => {
-    // Lógica para voltar, como redirecionar ou fechar uma página
-    alert("Voltando...");
-  };
-  return (
-    <div className="app">
-      <Header
-        greeting="Voce ainda nao se identificou."
-        titleButton="(Acessar)"
-        showBellIcon={false}
-      />
-      {/* OU*/}
-      {/*  <Header greeting="Bem vindo," title="Gestao Escolar." titleButton="Sair" showBellIcon={true}/>*/}
-      <main>
-        <div id="test">
-          <Button text="Cadastrar" onClick={() => alert("Botão clicado!")} />
-        </div>
-        <div id="back-button">
-          <BackButton onClick={handleBack} />
-        </div>
-
-        <button onClick={abrirModal}>Abrir Modal</button>
-        <Modal
-          estaAberto={modalAberto}
-          aoFechar={fecharModal}
-          mensagem="Cadastro realizado com sucesso!"
-        />
-
-        <Input tipo="texto" />
-        <Input tipo="numero" largura="300px" />
-        <div id="dropdown">
-          <Dropdown titulo="Editar" itens={itensDropdown} />
-        </div>
-      </main>
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
