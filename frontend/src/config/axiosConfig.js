feed3978a35e13c195b7e3eae5b635644841daec
Cassio Renan <cassio.rr.de.paula@gmail.com>
2024-10-05 15:43:36 -0300

    Configuracao token csrf para o axios, criacao de form teste no componente home autentic

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,8 +1,36 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: "http://127.0.0.1:8000/api",
 });
+
+function obterCookie(nome) {
+  let valorCookie = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, nome.length + 1) === nome + "=") {
+        valorCookie = decodeURIComponent(cookie.substring(nome.length + 1));
+        break;
+      }
+    }
+  }
+  return valorCookie;
+}
+
+api.interceptors.request.use(
+  (config) => {
+    const csrftoken = obterCookie("csrftoken");
+    if (csrftoken) {
+      config.headers["X-CSRFToken"] = csrftoken;
+    }
+    return config;
+  },
+  (erro) => {
+    return Promise.reject(erro);
+  }
+);
 
 // api.interceptors.request.use(
 //   (config) => {
--- frontend/src/pages/home/homeAutentic.js
+++ frontend/src/pages/home/homeAutentic.js
@@ -4,6 +4,23 @@ import PageContainer from "../../components/PageContainer/PageContainer";
 import "./homeAutentic.css";
 
 const HomeAutentic = () => {
+  const envioHandler = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const mensagem = formData.get("teste").toString();
+
+    const params = { mensagem };
+
+    const res = await testeService.create(params);
+
+    if (res.status === 200)
+      alert(`A mensagem enviada ao servidor foi: "${res.data.mensagem}"`);
+
+    alert("Algo deu errado!");
+  };
+
   return (
     <PageContainer>
       <Header
@@ -12,7 +29,17 @@ const HomeAutentic = () => {
         }}
       />
       <div className="content">
-        <p>PARABÉNS, VOCÊ FOI AUTENTICADO!</p>
+        <h3>FORM TESTE</h3>
+        <form onSubmit={envioHandler}>
+          <input
+            type="text"
+            name="teste"
+            onChange={(e) => {
+              e.value = e.target.value;
+            }}
+          />
+          <button type="submit">ENVIAR</button>
+        </form>
       </div>
       {/* Conteúdo da Home depois de autenticado */}
       <Footer />
