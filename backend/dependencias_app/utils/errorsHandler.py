eb7c4735fd3dac0b001fa1827ecc5ac0ee8ae2ea
Jeferson Machado <jeferson.4machado@gmail.com>
2024-10-09 18:44:33 -0300

    Comentando codigo

--- backend/dependencias_app/utils/errorsHandler.py
+++ backend/dependencias_app/utils/errorsHandler.py
@@ -4,16 +4,19 @@ from rest_framework import status
 from functools import wraps
 
 def handle_view_errors(view_func):
+    """
+    Decorator que captura e trata erros que ocorrem na execução de uma view.
+    """
     @wraps(view_func)
     def wrapper(*args, **kwargs):
         try:
             return view_func(*args, **kwargs)
-        except ValidationError as e:
+        except ValidationError as e: # Captura erros de validação e retorna uma resposta de erro com status 400.
             return Response({
                 "error": "Erro de validação",
                 "details": e.detail
             }, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
+        except Exception as e: # Captura qualquer outro erro inesperado e retorna uma resposta de erro com status 500
             return Response({
                 "error": "Erro inesperado",
                 "details": str(e)
