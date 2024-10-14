a9be497111b3f2c3dde65bd87647abe27165a136
Carolaine Machado <carolainemachado12345@gmail.com>
2024-10-13 21:53:00 -0300

    Atualizado classe de turma e curso e serializer de curso

--- backend/dependencias_app/models/curso.py
+++ backend/dependencias_app/models/curso.py
@@ -1,17 +1,13 @@
 from django.db import models
 from .modalidade import Modalidade
 from dependencias_app.models.base import *
-from dependencias_app.models.turma import Turma
 from django.core.validators import MinLengthValidator
 
 class Curso(BaseModel):
-    nome = models.CharField(verbose_name="Nome do Curso",help_text="Informe o nome do curso",
-        max_length=100,validators=[MinLengthValidator(3)],null=False,blank=False)
-    carga_horaria = models.CharField(verbose_name="Carga Horária",help_text="Informe a carga horária do curso",
-        max_length=5,null=False,blank=False)
-    modalidade = models.CharField(verbose_name="Modalidade",help_text="Selecione a modalidade do curso",
-        choices=Modalidade.choices,max_length=20,null=False,blank=False)
-    #turma = models.ManyToManyField(Turma)
+    nome = models.CharField(verbose_name="Nome do Curso",help_text="Informe o nome do curso",max_length=100,validators=[MinLengthValidator(3)],null=False,blank=False)
+    carga_horaria = models.CharField(verbose_name="Carga Horária",help_text="Informe a carga horária do curso",max_length=5,null=False,blank=False)
+    modalidade = models.CharField(verbose_name="Modalidade",help_text="Selecione a modalidade do curso",choices=Modalidade.choices,max_length=20,null=False,blank=False)
+    
     
     class Meta:
         abstract = False
--- backend/dependencias_app/serializers/cursoSerializer.py
+++ backend/dependencias_app/serializers/cursoSerializer.py
@@ -6,15 +6,7 @@ class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
-
-# def create(self, validated_data):
-#         turmas_data = validated_data.pop('turmas')
-#         curso = Curso.objects.create(**validated_data)
-        
-#         for turma_data in turmas_data:
-#             Turma.objects.create(curso=curso, **turma_data)
-        
-#         return curso        
+     
 
       
 
