c74961bee008285e8459b089eee2016e9ffe7d8c
Carolaine Machado <carolainemachado12345@gmail.com>
2024-11-02 16:29:26 -0300

    Ajustes realizados em models modalidade, cursoSerializer, cadastroCurso.js, listaCurso js e css para Funcionar a lista de curso

--- backend/dependencias_app/models/modalidade.py
+++ backend/dependencias_app/models/modalidade.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Modalidade(models.TextChoices):
     INTEGRADO = 'Integrado', 'Educação de Jovens e Adultos (EMI)'
-    PROEJA = 'PROEJA', 'Educação de Jovens e Adultos com Ensino Profissionalizante'
+    PROEJA = 'ProEJA', 'Educação de Jovens e Adultos com Ensino Profissionalizante'
