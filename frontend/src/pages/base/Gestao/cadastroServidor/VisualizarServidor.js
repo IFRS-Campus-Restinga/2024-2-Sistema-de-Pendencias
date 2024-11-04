2ac63e38a5188c77d2ba3ab6947db7c2f5b8d3d6
Caroline Silva <carolinedsd@hotmail.com>
2024-11-04 14:26:30 -0300

    estilização da página de detalhes do servidor

--- frontend/src/pages/base/Gestao/cadastroServidor/VisualizarServidor.js
+++ frontend/src/pages/base/Gestao/cadastroServidor/VisualizarServidor.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Button from '../../../../components/Button/Button';
+import "./VisualizarServidor.css";
 
 const VisualizarServidor = () => {
     const location = useLocation();
+    const navigate = useNavigate();
     const { servidor } = location.state || {};  // Recupera o estado passado
-  
+
     if (!servidor) {
-      return <div>Nenhum servidor foi selecionado.</div>;
+        return <div>Nenhum servidor foi selecionado.</div>;
     }
 
     return (
-        <div>
+        <div className="visualizar-servidor-container">
             <h1>Detalhes do Servidor</h1>
-            <p><strong>Nome:</strong> {servidor.nome}</p>
-            <p><strong>Email:</strong> {servidor.email}</p>
-            <p><strong>Perfil:</strong> {servidor.perfil}</p>
+            <p className="detalhe-servidor"><strong>Nome:</strong> {servidor.nome}</p>
+            <p className="detalhe-servidor"><strong>Email:</strong> {servidor.email}</p>
+            <p className="detalhe-servidor"><strong>Perfil:</strong> {servidor.perfil}</p>
         </div>
     );
 };
