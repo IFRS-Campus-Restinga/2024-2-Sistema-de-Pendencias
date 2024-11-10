98199528544c3738a4246f1ee2d6905672db5379
Carolaine Machado <carolainemachado12345@gmail.com>
2024-11-09 21:28:14 -0300

    Ajuste para aparecer as opções nos filtros de cadastro do plano de estudos, preciso ajustar o campo aluno que não esta buscando

--- backend/dependencias_app/models/planoEstudos.py
+++ backend/dependencias_app/models/planoEstudos.py
@@ -3,6 +3,7 @@ from .base import BaseModel
 from dependencias_app.models.dependencia import *
 from dependencias_app.enums.formaOferta import FormaOferta
 from dependencias_app.enums.turnos import Turnos
+from dependencias_app.enums.trimestreRec import TrimestreRec
 
 
 class PlanoEstudos(BaseModel):
@@ -10,7 +11,7 @@ class PlanoEstudos(BaseModel):
     semestre_ano_letivo = models.CharField(verbose_name="Semestre/Ano Letivo", help_text="Exemplo: 2024/1", max_length=5, null=False, blank=False)
     componente_curricular = models.CharField(verbose_name="Componente Curricular", help_text="Informe o componente curricular", max_length=100, null=False, blank=False)
     semestre_serie_curso = models.CharField(verbose_name="Semestre/Série do Curso", help_text="Informe o semestre/série", max_length=20, null=False, blank=False)
-    trimestre_recuperar = models.CharField(verbose_name="Trimestre a Recuperar", help_text="Informe o trimestre", max_length=20, null=False, blank=False)
+    trimestre_recuperar = models.CharField(verbose_name="Trimestre a Recuperar", help_text="Informe o trimestre",choices=TrimestreRec.choices, max_length=20, null=False, blank=False)
     forma_oferta = models.CharField(verbose_name="Forma de Oferta", help_text="Selecione a forma de oferta", choices=FormaOferta.choices, max_length=20, null=False, blank=False)
     turno = models.CharField(verbose_name="Turno", help_text="Selecione o turno", choices=Turnos, max_length=10, null=False, blank=False)
     parecer_pedagogico = models.TextField(verbose_name="Parecer Pedagógico", help_text="Informe o parecer pedagógico",blank=False, null=False, max_length=500)
