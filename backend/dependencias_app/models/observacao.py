b0ff32847d6902016bea789bb9d103cefd413b7a
Carolaine Machado <carolainemachado12345@gmail.com>
2024-12-04 12:33:11 -0300

    Alteração no model

--- backend/dependencias_app/models/observacao.py
+++ backend/dependencias_app/models/observacao.py
@@ -1,12 +1,14 @@
 from django.db import models
 from .base import BaseModel
 from django.core.validators import MinLengthValidator
+from dependencias_app.models.base import *
+from django.db import models
+from dependencias_app.enums.statusDependencia import *
 
 class Observacao(BaseModel):
     data_insercao = models.DateTimeField(auto_now_add=True, verbose_name="Data de Inserção")
     parecer = models.TextField(verbose_name="Parecer", help_text="Informe o parecer da observação", blank=False, null=False)
-    status = models.CharField(max_length=50, choices=[('pendente', 'Pendente'), ('concluída', 'Concluída'), ('em andamento', 'Em andamento')], default='pendente', verbose_name="Status")
-    acoes = models.TextField(verbose_name="Ações", help_text="Ações tomadas em relação à observação", blank=True, null=True)
+    status = models.CharField(max_length=50, choices=StatusDependencia.choices)
 
     class Meta:
         verbose_name_plural = "Observações"
