65271f68150f7b50623dbf7c5d76d988f55ec79e
Maur0c0 <2020007989@restinga.ifrs.edu.br>
2024-12-07 17:02:45 -0300

    ajustando estilização detalhes ped aluno

--- frontend/src/pages/base/Aluno/detalhesPED/DetalhesPEDAluno.js
+++ frontend/src/pages/base/Aluno/detalhesPED/DetalhesPEDAluno.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 import FormContainer from "../../../../components/FormContainer/FormContainer";
+import StatusBalls from "../../../../components/StatusBall/StatusBall";
 import "./DetalhesPEDAluno.css";
 import { PEDService } from "../../../../services/pedService";
 import { jwtDecode } from "jwt-decode";
@@ -9,6 +10,7 @@ const DetalhesPEDAluno = () => {
   const [detalhesPED, setDetalhesPED] = useState({});
   const { pedId, modalidade: modalidadeParam } = useParams();
   const location = useLocation();
+  const navigate = useNavigate();
   const modalidade = location.state?.modalidade || modalidadeParam; // Captura do state ou da URL
   const usuarioId = jwtDecode(sessionStorage.getItem("token")).idUsuario;
 
@@ -30,25 +32,58 @@ const DetalhesPEDAluno = () => {
     fetchDetalhes();
   }, [pedId, modalidade]);
 
-  if (!detalhesPED) {
+  const handleVoltar = () => {
+    navigate("/sessao/Aluno/home");
+  };
+
+  if (!detalhesPED || Object.keys(detalhesPED).length === 0) {
     return <div>Carregando detalhes...</div>;
   }
 
   return (
-    <FormContainer titulo={`Detalhes da PED - ${modalidade}`} comprimento="90%">
+    <FormContainer titulo={`Detalhes da PED - ${modalidade === "Integrado" ? "Integrado" : "ProEJA"}`} comprimento="90%">
+      <label className="labelCabecalhoDetalhesPED">
+        <span className="labelStatusPED">Andamento da PED</span>
+      </label>
+
       <section className="sectionDetalhesPED">
-        <div className="divDetalhesPED">
+        <div className="informacoesContainer">
           <label className="labelDetalhesPED">
-            Docente responsável
+            Aluno
+            <p className="pDetalhesPED">{detalhesPED.aluno || "Não informado"}</p>
+          </label>
+          <label className="labelDetalhesPED">
+            Curso
+            <p className="pDetalhesPED">{detalhesPED.curso || "Não informado"}</p>
+          </label>
+          <label className="labelDetalhesPED">
+            Data de Início
+            <p className="pDetalhesPED">{detalhesPED.data_inicio || "Não informado"}</p>
+          </label>
+          <label className="labelDetalhesPED">
+            Data de Fim
+            <p className="pDetalhesPED">{detalhesPED.data_fim || "Não informado"}</p>
+          </label>
+          <label className="labelDetalhesPED">
+            Docente Responsável
             <p className="pDetalhesPED">{detalhesPED.professor_ped || "Não informado"}</p>
           </label>
-          <label className="labelDetalhesPED">
-            Disciplina
-            <p className="pDetalhesPED">{detalhesPED.disciplina || "Não informado"}</p>
-          </label>
         </div>
-        <div className="divStatusPED">
-          <p className="status">{detalhesPED.status}</p>
+
+        <div className="divStatusContainer">
+          <StatusBalls status={detalhesPED.status} tipo={"PED"} />
+          <div className="buttonsContainer">
+            <button className="btnVoltar" onClick={handleVoltar}>
+              Voltar
+            </button>
+            <button
+                className="btnPlanoEstudos"
+                disabled={!detalhesPED.plano_estudos} // Desativa o botão se plano_estudos for null
+                onClick={() => navigate(`/sessao/Aluno/${usuarioId}/plano-estudos/${pedId}`)}
+            >
+              Plano de Estudos
+            </button>
+          </div>
         </div>
       </section>
     </FormContainer>
